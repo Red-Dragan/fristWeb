9bdf22fc940b5cd203de4ac3f5219ff9258a800f
Sohel <sohelislamblg01@gmail.com>
2022-07-18 21:52:54 +0530

    message

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const path=require('path');
+const hbs=require('hbs');
+const app=express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, "../template/views"))
+hbs.registerPartials(path.join(__dirname, '../template/partials'))
+app.set('view engine', 'hbs');
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+app.get('/about', (req, res)=>{
+    res.render('about');
+})
+app.get('/contact', (req, res)=>{
+    res.render('contact');
+})
+app.get('*', (req, res)=>{
+    res.render('404');
+})
+app.listen(8000, (()=>{console.log("server is running")}))
